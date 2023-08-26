bfa4d82aaa4a52722b5ff2939cfe8fd0724e5bb8
Serhii <mel.gipppson@gmail.com>
2023-08-26 20:47:34 +0300

    Sections Feedback and Sertificates completed

--- js/main.js
+++ js/main.js
@@ -5,3 +5,30 @@ menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu--open')
 })
 
+const feedbackSwiper = new Swiper('.feedback__slider', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+const certificatesSwiper = new Swiper('.certificates__slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 3,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      360: {
+        slidesPerView: 1,
+      },
+    }
+  });
+
